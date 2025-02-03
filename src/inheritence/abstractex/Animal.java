b9c0d2f4049f32f9a55a9a85f2978adb5977ef44
thatzcool <mycaptain0771@gmail.com>
2025-02-03 16:29:00 +0900

    day01_end

---
+++ src/inheritence/abstractex/Animal.java
@@ -0,0 +1,8 @@
+package inheritence.abstractex;
+
+public abstract class Animal {
+    public abstract void sound();   //추상 메서드
+    public void breathe(){
+        System.out.println("동물은 숨을 쉽니다.");
+    }
+}
